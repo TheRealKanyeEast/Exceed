82078b3269fcf00dc8af834f091c5c5f4bbf9e02
Kanye East <79680478+TheRealKanyeEast@users.noreply.github.com>
2023-10-14 10:35:13 +0530

    Update LOD.lua

--- LOD.lua
+++ LOD.lua
@@ -2,9 +2,9 @@ if getgenv and getfenv and getsenv and setclipboard and hookfunction then
     if game.PlaceId == 13698105873 or game.PlaceId == 14372616578 then -- Skibidi Toilet Attack, Hard Mode
         print("Exceed.gg : Skibidi Toilet Attack");
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealKanyeEast/Exceed/main/STA.lua'))()
-    elseif game.PlaceId == 3623096087 then -- Muscle Legends
-        print("Exceed.gg : Muscle Legends");
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealKanyeEast/Exceed/main/ML.lua'))()
+    --elseif game.PlaceId == 3623096087 then -- Muscle Legends
+        --print("Exceed.gg : Muscle Legends");
+		--loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealKanyeEast/Exceed/main/ML.lua'))()
     elseif game.PlaceId == 1537690962 then -- Bee Swarm Simulator
         print("Exceed.gg : Bee Swarm Simulator");
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/TheRealKanyeEast/Exceed/main/BSS.lua'))()
